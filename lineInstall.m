23be0c47e536967f73f3cc077e76c50d76f2200c
Giuliano Casale <g.casale@imperial.ac.uk>
2022-05-17 13:38:09 +0100

    fix: bug in lineInstall.m

--- lineInstall.m
+++ lineInstall.m
@@ -1,17 +1,17 @@
 w=warning('query');
 warning on
-disp('Checking java...')
+disp('Checking JAVA...')
 [status,result] = system('java');
 hasWarnings = false;
-if result == 0
-    error('ERROR: the Java Runtime Environment is not installed, this is required for LINE.')
+if status == 0
+    error('ERROR: the Java Runtime Environment (JRE) is not installed, this is required for LINE.')
 end
-disp('Checking MATLAB dependencies...')
+disp('Checking MATLAB toolboxes...')
 if license('test','statistics_toolbox')==0
-    error('ERROR: the Statistics and Machine Learning toolbox is not installed, this is required for LINE.')
+    warning('ERROR: the Statistics and Machine Learning toolbox is not installed, this is required for LINE.')
 end
 if license('test','optimization_toolbox')==0
-    error('ERROR: the Optimization Toolbox is not installed, this is required for LINE.')
+    warning('ERROR: the Optimization Toolbox is not installed, this is required for LINE.')
 end
 if license('test','gads_toolbox')==0
     warning('The Global Optimization Toolbox is not installed, this is required for LINE.')
@@ -23,11 +23,12 @@ if license('test','symbolic_toolbox')==0
 end
 disp('Checking LQNS...')
 [status,result] = system('lqns --h');
-if result == 0
-    warning('WARNING: LQNS is not installed, this may be required by some LINE methods.')
+if (isunix & result == 127) | (ispc & result > 0) % command not found
+    warning('WARNING: LQNS is not installed, this may be required by some LINE methods. Download it at: https://github.com/layeredqueuing/dist')
     hasWarnings = true;
 end
 disp('Checking JMT...')
+lineStart;
 jmtGetPath;
 warning(w);
 if hasWarnings
