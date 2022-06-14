4ff2e9a7c9a60183be2470e8590706f646be3d46
Giuliano Casale <g.casale@imperial.ac.uk>
2022-06-14 08:22:46 +0530

    fix: line installer

--- lineInstall.m
+++ lineInstall.m
@@ -1,3 +1,5 @@
+cwd = fileparts(mfilename('fullpath'));
+addpath(genpath(cwd));
 w=warning('query');
 warning on
 disp('Checking JAVA...')
@@ -8,21 +10,21 @@ if status == 0
     error('ERROR: the Java Runtime Environment (JRE) is not installed, this is required for LINE.')
 end
 disp('Checking MATLAB toolboxes...')
-if none(strcmp('Statistics and Machine Learning Toolbox', {v.Name}))
+if ~any(strcmp('Statistics and Machine Learning Toolbox', {v.Name}))
     warning('ERROR: the Statistics and Machine Learning toolbox is not installed, this is required for LINE.')
 end
-if none(strcmp('Optimization Toolbox', {v.Name}))
+if ~any(strcmp('Optimization Toolbox', {v.Name}))
     warning('ERROR: the Optimization Toolbox is not installed, this is required for LINE.')
 end
-if none(strcmp('Global Optimization Toolbox', {v.Name}))
+if ~any(strcmp('Global Optimization Toolbox', {v.Name}))
     warning('The Global Optimization Toolbox is not installed, this is required for LINE.')
     hasWarnings = true;
 end
-if none(strcmp('Parallel Computing Toolbox', {v.Name}))
+if ~any(strcmp('Parallel Computing Toolbox', {v.Name}))
     warning('The Parallel Computing Toolbox is not installed, this is required for LINE.')
     hasWarnings = true;
 end
-if none(strcmp('Symbolic Math Toolbox ', {v.Name}))
+if ~any(strcmp('Symbolic Math Toolbox ', {v.Name}))
     warning('The Symbolic Math Toolbox is not installed, this may be required by some LINE methods.')
     hasWarnings = true;
 end
